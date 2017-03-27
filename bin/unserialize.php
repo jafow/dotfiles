4025ec2e210e8a77c9f5364715d1202a86ba1f47
jafow <jared.a.fowler@gmail.com>
2017-03-27 09:40:44 -0700

    add some work scripts

---
+++ bin/unserialize.php
@@ -0,0 +1,18 @@
+<?php
+
+$usage = "php unserialize.php <filename> <destination>\n";
+
+if($argc < 2){
+    echo $usage;
+    die;
+}
+
+if($argc < 3) {
+    echo $usage;
+    die;
+}
+$string_unserialized = unserialize(file_get_contents($argv[1]));
+
+$json = json_encode($string_unserialized , JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+
+file_put_contents($argv[2] , $string_unserialized);
