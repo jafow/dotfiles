d7db157f9a6050ddc1e19cbd2169b7df780b9015
jafow <jared.a.fowler@gmail.com>
2017-11-24 22:21:56 -0800

    Install latest LTS nodejs version (8.9)
            add more useful global installs

--- npm.sh
+++ npm.sh
@@ -1,14 +1,15 @@
+#! /usr/bin/env bash
+
 # get nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
 
-nvm install 6.2.0
-nvm use 6.2.0
+nvm install 8.9.0
+nvm use 8.9.0
 
 # global npm packages
 packages=(
-  gulp
-  stream-adventure
-  webpack
+    eslint,
+    dat
 )
 
 npm i -g "${packages[@]}"
